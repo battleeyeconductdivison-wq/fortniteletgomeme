5237b1ccba2f03a57ee0b1ad95aecd564b7cc7d7
battleeyeconductdivison-wq <battleeyeconductdivison@gmail.com>
2025-09-02 18:38:47 -0400

    Update updated.luaa

--- updated.lua
+++ updated.lua
@@ -711,7 +711,7 @@ end
                 Name = "\0";
                 TextColor3 = themes.preset.accent;
                 BorderColor3 = rgb(0, 0, 0);
-                Text = '<font color="rgb(72, 72, 73)">' .. playeruser .. ' | Welcome To ' .. cfg.name .. '</font>'
+                Text = '<font color="rgb(72, 72, 73)">' .. player.Name .. ' | Welcome To ' .. cfg.name .. '</font>'
                 Size = dim2(1, 0, 0, 0);
                 Position = dim2(0, -10, 0.5, -1);
                 AnchorPoint = vec2(0, 0.5);
